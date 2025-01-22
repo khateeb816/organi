b74cbc1f6c4b79f2a1844313a12ace9ee88c957b
hasnadsatti <hasnadsatti740@gmail.com>
2025-01-22 17:36:20 +0500

    frontend done

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -11,4 +11,37 @@ class HomeController extends Controller
     public function home(){
         return view('frontend.home.index');
     }
+
+    public function shopGrid(){
+        return view('frontend.shop.index');
+    }
+
+    public function shopDetails(){
+        return view('frontend.shop.shopDetails');
+    }
+
+    public function shopingCart(){
+        return view('frontend.shop.shopingCart');
+    }
+
+
+    public function checkout(){
+        return view('frontend.checkout.index');
+    }
+
+
+    public function blogDetails(){
+        return view('frontend.blogs.blogDetails');
+    }
+
+
+    public function blog(){
+        return view('frontend.blogs.index');
+    }
+
+
+    public function contact(){
+        return view('frontend.contact.index');
+    }
+
 }
--- resources/views/frontend/components/footer.blade.php
+++ resources/views/frontend/components/footer.blade.php
@@ -63,3 +63,17 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
         </div>
     </div>
 </footer>
+<script src="{{ asset('frontendAssets/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{ asset('frontendAssets/js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('frontendAssets/js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{ asset('frontendAssets/js/jquery-ui.min.js')}}"></script>
+    <script src="{{ asset('frontendAssets/js/jquery.slicknav.js')}}"></script>
+    <script src="{{ asset('frontendAssets/js/mixitup.min.js')}}"></script>
+    <script src="{{ asset('frontendAssets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('frontendAssets/js/main.js')}}"></script>
+
+
+
+</body>
+
+</html>
